06aa550c3dd6133d279fc454ca8cb05d16e6abc0
Zeeshan Khan <zeeshan4ev@gmail.com>
2025-08-22 17:31:14 +0500

    First commit - added counter project

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-counter',
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+  counter: number = 0;
+  increment() {
+    this.counter++;
+  }
+  decrement() {
+    this.counter--;
+  }
+  reset() {
+    this.counter = 0;
+  }
+
+  getCounterStatus(): string {
+    if (this.counter > 0) {
+      return 'positive';
+    } else if (this.counter < 0) {
+      return 'negative';
+    } else {
+      return 'neutral';
+    }
+  }
+}
